2bdfb17339e344ebaaef73c3e49ad16e6e682246
Senay Mustafa <senaim98@gmail.com>
2024-09-19 16:02:05 +0200

    Running prettier to satisfy preflight requirements

--- src/App.js
+++ src/App.js
@@ -114,7 +114,8 @@ export default function App() {
               display: 'block',
               marginBottom: '10px',
             }}
-          >Meme Template
+          >
+            Meme Template
           </label>
           <input
             id="tempInput"
